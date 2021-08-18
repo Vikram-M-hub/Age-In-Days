f221bdb222624f2fa2c3f682023107e588878142
Vikram M <82879308+Vikram-M-hub@users.noreply.github.com>
2021-08-18 11:22:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+//console.log('Hi');
+
+function ageInDays() {
+    var birthYear = prompt('What year where you born?');
+    var currentYear = prompt('Which year is it now?')
+    var ageInDayss = (currentYear - birthYear)*365;
+    var h1 = document.createElement('h1');
+    var textAnswer = document.createTextNode('You are ' + ageInDayss + ' days old!!!');
+    h1.setAttribute('id','ageInDays');
+    h1.appendChild(textAnswer);
+    document.getElementById('flex-box-result').appendChild(h1);
+}
+
+function reset() {
+    document.getElementById('ageInDays').remove();
+}
